2a4e8ac7fe44c948244821d43b5b56ca3b73b161
jjohn273 <jjohn273@node056.cm.cluster>
2020-02-08 21:01:17 -0500

    init threshold monitoring

--- CustomCallbacks.py
+++ CustomCallbacks.py
@@ -1,7 +1,12 @@
 from sklearn.metrics import roc_auc_score
 import numpy as np
 import tensorflow as tf
+import sys
 Callback = tf.keras.callbacks.Callback
+ecbdl14_root = '/home/jjohn273/git/ECBDL14-Classification/'
+sys.path.append(ecbdl14_root)
+from utils import get_best_threshold
+
 
 class KerasRocAucCallback(Callback):
     def __init__(self, x, y, is_validation=False, logger=None):
@@ -19,3 +24,16 @@ class KerasRocAucCallback(Callback):
         logs[self.metric_key] = auc
         if self.logger != None:
             self.logger.log_time(f'Epoch: {epoch}  {self.metric_key}: {auc}').write_to_file()
+
+class KerasThresholdMonitoringCallback(Callback):
+    def __init__(self, x, y, logger=None):
+        super(Callback, self).__init__()
+        self.x = x
+        self.y = y
+        self.logger = logger
+
+    def on_epoch_end(self, epoch, logs={}, generator=None):
+        optimal_threshold = get_best_threshold(self.x, self.y, self.model)
+        logs['optimal_threshold'] = optimal_threshold
+        if self.logger != None:
+            self.logger.log_time(f'Completed run with threshold: {optimal_threshold}').write_to_file()
--- tests/evaluator.py
+++ tests/evaluator.py
@@ -10,76 +10,9 @@ from sklearn.exceptions import UndefinedMetricWarning
 import warnings
 import numpy as np
 import math
-
-# caused by divide by zero during metrics calcultiong
-# ignoring  because it is saturating the error logs
-warnings.filterwarnings(action='ignore', category=UndefinedMetricWarning)
-
-
-
-def load_model(model_json, weights_path):
-    model = model_from_json(model_json)
-    model.load_weights(weights_path)
-    return model
-
-
-def rounded_str(num, precision=6):
-    if type(num) is str:
-        return num
-    return str(round(num, precision))
-
-
-def get_best_threshold(train_x, train_y, model, delta=0.005):
-    curr_thresh = 0.0
-    best_thresh = 0.0
-    best_gmean = 0.0
-
-    y_prob = model.predict(train_x)
-
-    while True:
-        y_pred = np.where(y_prob > curr_thresh, np.ones_like(y_prob), np.zeros_like(y_prob))
-        tn, fp, fn, tp = confusion_matrix(train_y, y_pred).ravel()
-        tpr = (tp) / (tp + fn)
-        tnr = (tn) / (tn + fp)
-        if tnr > tpr:
-            return best_thresh
-        gmean = math.sqrt(tpr*tnr)
-        if gmean > best_gmean:
-            best_gmean = gmean
-            best_thresh = curr_thresh
-        curr_thresh += delta
-
-    return best_thresh
-
-
-
-columns = ['minority_size', 'strategy', 'threshold', 'tp', 'fp', 'tn', 'fn', 'tpr', 'tnr', 'roc_auc', 'geometric_mean', 'arithmetic_mean', 'f1_score', 'precision']
-
-
-def write_performance_metrics(y_true, y_prob, minority_size, strategy, threshold, path):
-    # include header if file doesn't exist yet
-    out = ",".join(columns) if not os.path.isfile(path) else ''
-
-    predictions = np.where(y_prob > threshold, 1.0, 0.0)
-    tn, fp, fn, tp = confusion_matrix(y_true, predictions).ravel()
-    tpr = (tp) / (tp + fn)
-    tnr = (tn) / (tn + fp)
-    roc_auc = roc_auc_score(y_true, y_prob)
-    geometric_mean = math.sqrt(tpr * tnr)
-    arithmetic_mean = 0.5 * (tpr + tnr)
-    f1 = f1_score(y_true, predictions)
-    precision = precision_score(y_true, predictions)
-
-    results = [minority_size, strategy, threshold,
-               tp, fp, tn, fn, tpr, tnr, roc_auc,
-               geometric_mean, arithmetic_mean, f1, precision]
-
-    results = [rounded_str(x) for x in results]
-
-    out += '\n' + ','.join(results)
-
-    with open(path, 'a') as outfile:
-        outfile.write(out)
+ecbdl14_root = '/home/jjohn273/git/ECBDL14-Classification/'
+sys.path.append(ecbdl14_root)
+from utils import load_model, rounded_str, get_best_threshold, write_performance_metrics
 
 
 # input config
